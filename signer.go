cf17845beebe01c7fe7b0574de1f95fc7e8d440c
Ato Araki <ato.araki@groove-x.com>
2019-04-11 21:52:06 +0900

    Support sha384

--- signer.go
+++ signer.go
@@ -18,6 +18,8 @@ type Signer struct {
 	client      *kms.KeyManagementClient
 	signTimeout time.Duration
 
+	algorithm kmspb.CryptoKeyVersion_CryptoKeyVersionAlgorithm
+
 	pubKey crypto.PublicKey
 }
 
@@ -42,6 +44,7 @@ func NewSigner(client *kms.KeyManagementClient, keyPath string) (*Signer, error)
 		keyPath:     keyPath,
 		client:      client,
 		signTimeout: 15 * time.Second,
+		algorithm:   pubKeypb.Algorithm,
 		pubKey:      pubKey,
 	}, nil
 }
@@ -54,13 +57,31 @@ func (s *Signer) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) (si
 	ctx := context.Background()
 	ctx, _ = context.WithTimeout(context.Background(), s.signTimeout)
 
-	res, err := s.client.AsymmetricSign(ctx, &kmspb.AsymmetricSignRequest{
-		Name: s.keyPath,
-		Digest: &kmspb.Digest{
+	var kmsDigest *kmspb.Digest
+
+	switch s.algorithm {
+	case kmspb.CryptoKeyVersion_RSA_SIGN_PKCS1_2048_SHA256,
+		kmspb.CryptoKeyVersion_RSA_SIGN_PKCS1_3072_SHA256,
+		kmspb.CryptoKeyVersion_RSA_SIGN_PKCS1_4096_SHA256,
+		kmspb.CryptoKeyVersion_EC_SIGN_P256_SHA256:
+		kmsDigest = &kmspb.Digest{
 			Digest: &kmspb.Digest_Sha256{
 				Sha256: digest,
 			},
-		},
+		}
+	case kmspb.CryptoKeyVersion_EC_SIGN_P384_SHA384:
+		kmsDigest = &kmspb.Digest{
+			Digest: &kmspb.Digest_Sha384{
+				Sha384: digest,
+			},
+		}
+	default:
+		return nil, fmt.Errorf("not implemented yet: %s", s.algorithm.String())
+	}
+
+	res, err := s.client.AsymmetricSign(ctx, &kmspb.AsymmetricSignRequest{
+		Name:   s.keyPath,
+		Digest: kmsDigest,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to sign: %s", err)
